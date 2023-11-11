c01796eecd58fd3d73bdc7c0df6e2eb6e066ebca
Ann <127487362+ann-strn@users.noreply.github.com>
2023-11-11 11:42:50 +0300

    task2

--- src/memset.c
+++ src/memset.c
@@ -1,10 +1,10 @@
 // 4. Копирует символ c (беззнаковый тип) в первые n символов строки, на которую
 //  указывает аргумент str
 
-#include "s21_string.h"
+#include "string.h"
 
-void *s21_memset(void *str, int c, s21_size_t n) {
-  for (s21_size_t i = 0; i < n; i++) {
+void *memset(void *str, int c, size_t n) {
+  for (size_t i = 0; i < n; i++) {
     *((char *)str + i) = c;
   }
   return str;
