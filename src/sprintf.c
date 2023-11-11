3afb940c4868b1e4b5a4303876ed8db7ad575757
Ann <127487362+ann-strn@users.noreply.github.com>
2023-11-11 11:43:42 +0300

    task2

--- src/sprintf.c
+++ src/sprintf.c
@@ -1,6 +1,6 @@
-#include "s21_sprintf.h"
+#include "sprintf.h"
 
-int s21_sprintf(char *str, const char *format, ...) {
+int sprintf(char *str, const char *format, ...) {
   va_list ap;
   va_start(ap, format);
   char *tmp_str = str;
@@ -8,14 +8,14 @@ int s21_sprintf(char *str, const char *format, ...) {
   while (*tmp_format != '\0') {
     if (*tmp_format == '%') {
       pars_format form = {0, 0, 0, -1, -1, -1, -1, 0, 0};
-      tmp_format = s21_parser(tmp_format + 1, &form);
+      tmp_format = parser(tmp_format + 1, &form);
       if (form.percent) {
         *tmp_str = '%';
         tmp_str++;
         tmp_format++;
       } else {
-        char *buf = s21_transfer_buf(ap, &form);
-        int length_buf = s21_strlen(buf);
+        char *buf = transfer_buf(ap, &form);
+        int length_buf = strlen(buf);
         for (int i = 0; i < length_buf; i++) {
           *tmp_str = buf[i];
           tmp_str++;
@@ -30,6 +30,6 @@ int s21_sprintf(char *str, const char *format, ...) {
     if (*tmp_format == '\0') *tmp_str = *tmp_format;
   }
   va_end(ap);
-  int len = (int)s21_strlen(str);
+  int len = (int)strlen(str);
   return len;
 }
